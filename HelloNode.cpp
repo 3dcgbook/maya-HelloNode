87eab64b66056fd4a6ce122509b386d97696ef46
3dcgbook <3dcgbook@gmail.com>
2020-05-25 13:09:49 +0900

    Initial Commit.

---
+++ HelloNode.cpp
@@ -0,0 +1,145 @@
+#include <maya/MPxNode.h> 
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MTypeId.h> 
+#include <maya/MPlug.h>
+#include <maya/MDataBlock.h>
+#include <maya/MDataHandle.h>
+#include <maya/MEvaluationNode.h>
+#include <maya/MEvaluationManager.h>
+#include <maya/MFnPlugin.h>
+#include <maya/MGlobal.h>
+
+class HelloNode : public MPxNode
+{
+public:
+	HelloNode();
+	~HelloNode() override;
+	MStatus     compute(const MPlug& plug, MDataBlock& data) override;
+	MStatus     setDependentsDirty(const MPlug& plug, MPlugArray& plugArray) override;
+	MStatus     preEvaluation(const  MDGContext& context, const MEvaluationNode& evaluationNode) override;
+	static  void*       creator();
+	static  MStatus     initialize();
+public:
+	static  MObject     input1; // 入力１のアトリビュート
+	static  MObject     input2; // 入力２のアトリビュート
+	static  MObject     output; // 出力用のアトリビュート
+	static  MTypeId     id; // プラグインのID
+private:
+	double doCalculation(double& a, double& b);
+};
+
+// 静的な変数宣言
+MTypeId     HelloNode::id(0x80001);
+MObject     HelloNode::input1;
+MObject     HelloNode::input2;
+MObject     HelloNode::output;
+
+// クラスの定義
+HelloNode::HelloNode()
+{
+}
+
+HelloNode::~HelloNode()
+{
+}
+
+// 重要な部分その１：アトリビュートを作る！
+MStatus HelloNode::initialize()
+{
+	MFnNumericAttribute nAttr;
+
+	input1 = nAttr.create("input1", "in1", MFnNumericData::kDouble, 0.0);
+	input2 = nAttr.create("input2", "in2", MFnNumericData::kDouble, 0.0);
+	output = nAttr.create("output", "out", MFnNumericData::kDouble, 0.0);
+
+	nAttr.setWritable(false);
+	nAttr.setStorable(false);
+
+	addAttribute(input1);
+	addAttribute(input2);
+	addAttribute(output);
+
+	attributeAffects(input1, output);
+	attributeAffects(input2, output);
+
+	return MS::kSuccess;
+}
+
+
+// 重要な部分その２：ノードの計算部分！
+MStatus HelloNode::compute(const MPlug& plug, MDataBlock& data)
+{
+	MStatus stat;
+	if (plug != output) {
+		return MS::kUnknownParameter;
+	}
+	
+	double input1_data = data.inputValue(input1).asDouble();
+	double input2_data = data.inputValue(input2).asDouble();
+		
+		
+	double output_value = doCalculation(input1_data, input2_data);
+		
+	data.outputValue(HelloNode::output).set(output_value);
+	data.setClean(plug);
+
+	return MS::kSuccess;
+}
+
+
+// 上記の実装で使われる、加算関数
+double HelloNode::doCalculation(double& a, double& b)
+{
+	return a + b;
+}
+
+// おまけ：プラグがダーティーになったかDG版
+MStatus HelloNode::setDependentsDirty(const MPlug& plug, MPlugArray& plugArray)
+{
+	if (plug == input1 || plug == input2)
+	{	
+		MGlobal::displayInfo("MGlobal::setDependentsDirty");
+	}
+	
+	return MS::kSuccess;
+}
+
+// おまけ：プラグがダーティーになったかパラレル版
+MStatus HelloNode::preEvaluation(const  MDGContext& context, const MEvaluationNode& evaluationNode)
+{
+	if (context.isNormal())
+	{
+		if (evaluationNode.dirtyPlugExists(input1) || evaluationNode.dirtyPlugExists(input2))
+		{
+			MGlobal::displayInfo("MGlobal::preEvaluation");
+		}
+	}
+	return MS::kSuccess;
+}
+void* HelloNode::creator()
+{
+	return new HelloNode();
+}
+
+// プラグインの登録部分
+MStatus initializePlugin(MObject obj)
+{
+	MStatus   status;
+	MFnPlugin plugin(obj, "3dcgbook.inc", "1.0", "Any");
+
+	status = plugin.registerNode(
+		"HelloNode",
+		HelloNode::id, HelloNode::creator,
+		HelloNode::initialize
+	);
+	return status;
+}
+
+// プラグインの解除部分
+MStatus uninitializePlugin(MObject obj)
+{
+	MStatus   status;
+	MFnPlugin plugin(obj);
+	status = plugin.deregisterNode(HelloNode::id);
+	return status;
+}
